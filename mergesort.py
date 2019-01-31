a030cc951a1913abd36706b93ee17608f09ba578
andrew <aplaceoutofthesun@gmail.com>
2019-01-31 16:19:19 +1100

    added mergesort.py

---
+++ mergesort.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#
+# mergesort.py - implementation of the mergesort algorithm
+
+
+def mergesort(arr):
+    """"""
+    pass
+
+def main():
+    seq = [48, 11, 45, 92, 32, 61, 65, 57, 29, 96]
+    print(mergesort(seq))
+
+if __name__ == "__main__":
+    main()
